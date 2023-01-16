d2a0cf02fba66f02f4ca3668d51ba6b81aac5384
Charlie Ontiveros <dev@charlietheprogrammer.com>
2023-01-15 17:42:08 -0800

    Added FluentValidation package. Refactoring Problem override to support FluentValidation. Added ValidationProblem override to convert FluentValidation errors to ModelStateDictionary. Recall that extra properties are also added to ValidationProblem BuberDinnerProblemDetailsFactory.

--- BuberDinner.Api/Controllers/ApiController.cs
+++ BuberDinner.Api/Controllers/ApiController.cs
@@ -1,7 +1,9 @@
 using BuberDinner.Api.Common;
 using ErrorOr;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace BuberDinner.Api.Controllers;
 
@@ -13,18 +15,61 @@ public class ApiController : ControllerBase
     // there is an error.
     protected IActionResult Problem(List<Error> errors)
     {
-        HttpContext.Items[HttpContextItemKeys.Errors] = errors;
         
-        var firstError = errors[0];
-
-        var statusCode = firstError.Type switch
+        // This needs to be refactored to handle different types of errors.
+        
+        // Handle validation errors only when NOT using Fluent Validation
+        // if (errors.Any(error => error.Type == ErrorType.Validation))
+        // {
+        //     // Extract all the validation errors and return those only
+        //     var validationErrors = errors.Where(error => error.Type == ErrorType.Validation);
+        //     // This is bad thing about globals. It's not intuitive to remember that setting
+        //     // this here will automagically get picked up by the custom Problem Detail Factory.
+        //     HttpContext.Items[HttpContextItemKeys.Errors] = validationErrors;
+        //     var statusCode = StatusCodes.Status400BadRequest;
+        //     return Problem(statusCode: statusCode);
+        // }
+        
+        // Handle service level errors only.
+        if (errors.Any(error => error.Type == ErrorType.Conflict))
         {
-            ErrorType.Conflict => StatusCodes.Status409Conflict,
-            ErrorType.Validation => StatusCodes.Status400BadRequest,
-            ErrorType.NotFound => StatusCodes.Status404NotFound,
-            _ => StatusCodes.Status500InternalServerError
-        };
+            // Extract all the validation errors and return those only
+            var validationErrors = errors.Where(error => error.Type == ErrorType.Conflict).ToList();
+            // This is bad thing about globals. It's not intuitive to remember that setting
+            // this here will automagically get picked up by the custom Problem Detail Factory.
+            HttpContext.Items[HttpContextItemKeys.Errors] = validationErrors;
+            var statusCode = StatusCodes.Status409Conflict;
+            return Problem(statusCode: statusCode);
+        }
+        
+        
+        // HttpContext.Items[HttpContextItemKeys.Errors] = errors;
+        //
+        // var firstError = errors[0];
+        //
+        // var statusCode = firstError.Type switch
+        // {
+        //     ErrorType.Conflict => StatusCodes.Status409Conflict,
+        //     ErrorType.Validation => StatusCodes.Status400BadRequest,
+        //     ErrorType.NotFound => StatusCodes.Status404NotFound,
+        //     _ => StatusCodes.Status500InternalServerError
+        // };
+        
+        return Problem(statusCode: StatusCodes.Status500InternalServerError); 
+    }
 
-        return Problem(statusCode: statusCode, title: firstError.Description); 
+    protected IActionResult ValidationProblem(ValidationResult validationResult)
+    {
+        // Need to convert the validation result to model state and return ValidationProblem
+        ModelStateDictionary modelState = new ModelStateDictionary();
+        
+        List<ValidationFailure> validationFailures = validationResult.Errors;
+
+        foreach (ValidationFailure error in validationFailures)
+        {
+            modelState.AddModelError(error.PropertyName, error.ErrorMessage);
+        }
+        
+        return ValidationProblem(modelState);
     }
 }
